a9f0e31ebf293971b1db0f9c2fac9496a8aa4cf1
DDENNEDD <112163260+DDENNEDD@users.noreply.github.com>
2023-01-17 16:54:57 +0300

    push

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -8,29 +8,29 @@ final class MovieQuizViewController: UIViewController, MovieQuizViewControllerPr
     @IBOutlet private var activityIndicator: UIActivityIndicatorView!
     @IBOutlet private var yesButton: UIButton!
     @IBOutlet private var noButton: UIButton!
-
+    
     private var presenter: MovieQuizPresenter!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter = MovieQuizPresenter(viewController: self)
     }
-
-
+    
+    
     @IBAction private func yesButtonClicked(_ sender: UIButton) {
         presenter.yesButtonClicked()
         yesButton.isEnabled = false
         showLoadingIndicator()
     }
-
+    
     @IBAction private func noButtonClicked(_ sender: UIButton) {
         presenter.noButtonClicked()
         noButton.isEnabled = false
         showLoadingIndicator()
     }
-
- 
-
+    
+    
+    
     func show(quiz step: QuizStepViewModel) {
         hideLoadingIndicator()
         imageView.layer.borderColor = UIColor.clear.cgColor
@@ -40,45 +40,44 @@ final class MovieQuizViewController: UIViewController, MovieQuizViewControllerPr
         yesButton.isEnabled = true
         noButton.isEnabled = true
     }
-
+    
     func show(quiz result: QuizResultsViewModel) {
         let message = presenter.resultMessage()
-
+        
         let alert = UIAlertController(
             title: result.title,
             message: message,
             preferredStyle: .alert)
         alert.view.accessibilityIdentifier = "Game Results"
-
-            let action = UIAlertAction(title: result.buttonText, style: .default) { [weak self] _ in
-                guard let self = self else { return }
-
-                self.presenter.restartGame()
-            }
-
+        
+        let action = UIAlertAction(title: result.buttonText, style: .default) { [weak self] _ in
+            guard let self = self else { return }
+            
+            self.presenter.restartGame()
+        }
+        
         alert.addAction(action)
-
+        
         self.present(alert, animated: true, completion: nil)
     }
-
+    
     func highlightImageBorder(isCorrectAnswer: Bool) {
         imageView.layer.masksToBounds = true
         imageView.layer.borderWidth = 8
         imageView.layer.borderColor = isCorrectAnswer ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
     }
-
+    
     func showLoadingIndicator() {
         activityIndicator.isHidden = false
         activityIndicator.startAnimating()
     }
-
+    
     func hideLoadingIndicator() {
         activityIndicator.isHidden = true
     }
-
+    
     func showNetworkError(message: String) {
         hideLoadingIndicator()
-
         let alert = UIAlertController(
             title: "Ошибка",
             message: message,
